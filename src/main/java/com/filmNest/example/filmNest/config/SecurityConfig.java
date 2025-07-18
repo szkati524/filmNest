c9e07673279cb4967f693eff3afbcf3569bbeafb
SebastianSzkat <szkatsebastian@Gmail.com>
2025-07-18 18:53:06 +0200

    commit 008

---
+++ src/main/java/com/filmNest/example/filmNest/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.filmNest.example.filmNest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/","/films","/series","/searchByTagsPage","/contact","styles.css","/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/",true)
+                        .permitAll()
+                )
+                .logout(logout-> logout.permitAll());
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/filmNest/example/filmNest/controller/MainController.java
+++ src/main/java/com/filmNest/example/filmNest/controller/MainController.java
@@ -1,13 +1,17 @@
 package com.filmNest.example.filmNest.controller;
 
+import com.filmNest.example.filmNest.Enums.MovieType;
 import com.filmNest.example.filmNest.dto.MovieDTO;
 import com.filmNest.example.filmNest.service.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -17,18 +21,49 @@ public class MainController {
     public MainController(MovieService movieService) {
         this.movieService = movieService;
     }
+
     @GetMapping("/")
-    public String homePage(Model model){
-        model.addAttribute("movies",movieService.getAllMovies());
+    public String homePage(Model model) {
+        model.addAttribute("movies", movieService.getAllMovies());
         return "main";
     }
+
     @GetMapping("/films")
-    public String getAllMoviesPage(Model model){
+    public String getAllMoviesPage(Model model) {
         List<MovieDTO> movies = movieService.getAllMovies()
                 .stream()
+                .filter(movie -> movie.getMovieType() == MovieType.FILM)
                 .sorted(Comparator.comparing(MovieDTO::getName))
                 .toList();
-        model.addAttribute("movies",movies);
+        model.addAttribute("movies", movies);
         return "films";
     }
-}
+
+    @GetMapping("/series")
+    public String showSeries(Model model) {
+        List<MovieDTO> seriesOnly = movieService.getAllMovies().stream()
+                .filter(movie -> movie.getMovieType() == MovieType.SERIAL)
+                .toList();
+        model.addAttribute("seriesList", seriesOnly);
+        return "series";
+    }
+
+    @GetMapping("/searchByTagsPage")
+    public String searchByTagsPage(@RequestParam(required = false) String tags, Model model) {
+        if (tags != null && !tags.isEmpty()) {
+            List<String> tagList = Arrays.stream(tags.split(","))
+                    .map(String::trim)
+                    .filter(s -> !s.isEmpty())
+                    .collect(Collectors.toList());
+            List<MovieDTO> movies = movieService.findMoviesByHashTags(tagList);
+            model.addAttribute("movies", movies);
+        }
+            return "searchByTagsPage";
+        }
+        @GetMapping("contact")
+        public String contactPage(Model model){
+        return "contact";
+        }
+    }
+
+
